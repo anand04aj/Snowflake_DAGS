0138baa064cd73894084e61e08e64407d44501d8
anand04aj <84134041+anand04aj@users.noreply.github.com>
2021-05-13 19:29:15 +0530

    Update test_DAG.py

--- DAGS/test_DAG.py
+++ DAGS/test_DAG.py
@@ -18,17 +18,17 @@ dag = DAG(
 
 
 
-def row_count(**context):
+def row_count(**kwargs):
     sleep(120)
-    #dwh_hook = SnowflakeHook(snowflake_conn_id="ocudm")
-    #result = dwh_hook.get_first("select count(*) from curo_medclaims")
+    dwh_hook = SnowflakeHook(snowflake_conn_id="ocudm")
+    result = dwh_hook.get_first("select count(*) from curo_medclaims")
     print("hello")
     sleep(120)
     logging.info("Number of rows in `curo_medclaims`  - %s", result[0])
 
 
 with dag:
-    get_count = PythonOperator(task_id="get_count", python_callable=row_count)
+    get_count = PythonOperator(task_id="get_count", python_callable=row_count,provide_context=True)
     sleep = BashOperator(task_id='sleep',
                          bash_command='sleep 30')
     print_hello = BashOperator(task_id='print_hello',
