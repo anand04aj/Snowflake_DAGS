bbe16cd670a327ae5fb85d7295ad2cc1bf7a4319
anand04aj <84134041+anand04aj@users.noreply.github.com>
2021-05-13 20:06:29 +0530

    Update test_DAG.py

--- DAGS/test_DAG.py
+++ DAGS/test_DAG.py
@@ -16,22 +16,30 @@ dag = DAG(
     dag_id="snowflake_connector_test", default_args=args, schedule_interval='@once'
 )
 
+create_insert_query = [
+    """create table public.test_table (amount number);""",
+    """insert into public.test_table values(1),(2),(3);"""
+]
 
-
-def row_count(**kwargs):
-    sleep(120)
-    dwh_hook = SnowflakeHook(snowflake_conn_id="ocudm")
-    result = dwh_hook.get_first("select count(*) from curo_medclaims")
-    print("hello")
-    sleep(120)
-    logging.info("Number of rows in `curo_medclaims`  - %s", result[0])
+#def row_count(**kwargs):
+#    sleep(120)
+#    dwh_hook = SnowflakeHook(snowflake_conn_id="ocudm")
+#    result = dwh_hook.get_first("select count(*) from curo_medclaims")
+#    print("hello")
+#    sleep(120)
+#    logging.info("Number of rows in `curo_medclaims`  - %s", result[0])
 
 
 with dag:
+    create_insert = SnowflakeOperator(
+        task_id="snowfalke_create",
+        sql=create_insert_query,
+        snowflake_conn_id="ocudm",
+    )
     get_count = PythonOperator(task_id="get_count", python_callable=row_count,provide_context=True)
     sleep = BashOperator(task_id='sleep',
                          bash_command='sleep 30')
     print_hello = BashOperator(task_id='print_hello',
                                bash_command='echo "hello"')
 
-print_hello >> sleep >> get_count
+print_hello >> sleep >> create_insert
